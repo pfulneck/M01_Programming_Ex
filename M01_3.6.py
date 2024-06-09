e1c221fdfbd059f5406020926fa8929e22ce5fe2
pfulneck <137308426+pfulneck@users.noreply.github.com>
2024-06-09 19:34:18 -0400

    Add files via upload

---
+++ M01_3.6.py
@@ -0,0 +1,17 @@
+'''
+Seconds in an hour calculator
+'''
+
+seconds_per_minute = 60
+minutes_per_hour = 60
+hours_per_day = 24
+
+seconds_per_hour = seconds_per_minute * minutes_per_hour
+
+seconds_per_day = seconds_per_hour * hours_per_day
+
+'''Hours per day calculation'''
+print(seconds_per_day / seconds_per_hour)
+
+'''Hours per day calculation pt 2'''
+print(seconds_per_day // seconds_per_hour)
